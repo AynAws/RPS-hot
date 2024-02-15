5ae4845c9349c63952f28fe2831f36f0a35b04de
GrimCreeperyt <grimcreeperyt@gmail.com>
2024-02-15 09:17:53 -0500

    Counters are responsive and in line with figma.

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let rps = ['rock', 'paper', 'scissors'];
 let randNum = Math.floor(Math.random()*rps.length);
 
 let compChoice = rps[randNum];
+
+let x = window.screen.width;
+console.log(x); // returns screen width in pixels
+
+if (x < 640) {
+    document.getElementById('left').classList.remove('right');
+    document.getElementById('d:none').classList.remove('smtext');
+} else {
+}
