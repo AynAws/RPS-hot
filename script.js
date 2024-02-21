b1ea0b20107c5363984459b2cbca9849829576ba
GrimCreeperyt <grimcreeperyt@gmail.com>
2024-02-21 09:05:43 -0500

    begna work on evaluating computer shoices vs player choices

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-let name = prompt('What is your name?'); // Collects and stores user name
+//let name = prompt('What is your name?'); // Collects and stores user name
+// COMPUTER CHOICE
 let shot = ['shot', 'mirror', 'shield', 'reload'];
 let rps = ['rock', 'paper', 'scissors'];
 let compList = rps;
-let time = 3; // The length of the timer in seconds
-let randNum = Math.floor(Math.random()*compList.length); // Generates a number to choose an option for the computer
+let randNum = Math.floor(Math.random()*compList.length); // generates a number to choose an option for the computer
 let compChoice = rps[randNum]; // decides what the computer chooses
+// HUMAN CHOICE
+let humanChoice; // defines the varibale that will story the user's choice
+// WINDOW WIDTH
 let x = window.screen.width; // assigns screen width to a variable known as x
-document.getElementById('timer').textContent = time; // places the timer in HTML doc
 console.log(x); // returns screen width in pixels
+// TIMER
+let time = 3; // the length of the timer in seconds
+document.getElementById('timer').textContent = time; // places the timer in HTML doc
 const stopTimer = () => clearInterval(interval); // stops the timer when called
+// COUNTERS
+let compCount = humanCount = roundCount = 0; // creates variables for the counters
+let humanWL = humanCount / compCount; // creates the W/L ratio counter
+document.getElementById('roundCount').textContent
 
-alert(`Welcome, ${name}!`)
+//alert(`Welcome, ${name}!`)
 
 if (x < 640) {
     document.getElementById('left').classList.remove('right');
@@ -29,3 +38,17 @@ function timer() {
     document.getElementById('timer').textContent = time;
 }; // adds one to the variable "time"
 const interval = setInterval(timer, 1000); // runs timer() every second
+
+console.log(compCount, humanCount, roundCount, humanWL);
+
+function game() {
+    function humanChooses() {
+    }
+    function compRock() {
+    }
+    function compPaper() {
+    }
+    function compScissors() {
+    }
+    roundCount++;
+}
