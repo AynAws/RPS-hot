fd19b05b87dfaf4fdf8465a8de519b163850d1f2
GrimCreeperyt <grimcreeperyt@gmail.com>
2024-02-27 09:22:56 -0500

    finished rps, just trying to fix displaying human and computer choices

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # WIN
-Gain a W/L ratio of 1.5 to win.
+Gain a W/L ratio of 4 to win.
 
 # LOSE
-Gain a W/L ratio of 0.5 to lose.
+Gain a W/L ratio of 0.25 to lose.
+
+Hold your winning or losing status for another round for it to take effect.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//let name = prompt('What is your name?'); // Collects and stores user name
+let name = prompt('What is your name?'); // Collects and stores user name
 // COMPUTER CHOICE (continues in newRound() func)
 let shot = ['shot', 'mirror', 'shield', 'reload']; // values for s-hot
 let rps = ['rock', 'paper', 'scissors']; // values for rock paper scissors
@@ -8,7 +8,7 @@ function randNum() {
     return Math.floor(Math.random()*compList.length);
 }; // generates a number to choose an option for the computer
 let compChoice;
-// HUMAN CHOICE (continues in humanChooses func)
+// HUMAN CHOICE (continues in roundEval func)
 let humanChoice; // defines the varibale that will story the user's choice
 // WINDOW WIDTH
 let x = window.screen.width; // assigns screen width to a variable known as x
@@ -22,9 +22,11 @@ let compCount = 1; // creates variables for computer win count
 let humanCount = 1; // creates variables for human win count count
 let roundCount = 0; // creates variables for round count
 let humanWL = humanCount / compCount; // creates the W/L ratio counter
+let end = false; // will be set to true when game ends
 
-//alert(`Welcome, ${name}!`)
+alert(`Welcome, ${name}!`)
 document.getElementById('userName').textContent = name;
+document.getElementById('humanName').textContent = name;
 
 // counters set in HTML
 document.getElementById('roundCount').textContent = roundCount;
@@ -40,12 +42,35 @@ if (x < 640) {
     document.getElementById('d:none').classList.add('smtext');
 }; // makes the counters change sides based off of screen size (upon refresh)
 
-console.log('Computer count:' + compCount, 'Human count:' + humanCount, 'Round count:' + roundCount, 'W/L ratio:' + humanWL); // returns 0, 0, 0, NaN
+console.log('Computer count: ' + compCount, 'Human count: ' + humanCount, 'Round count: ' + roundCount, 'W/L ratio: ' + humanWL); // returns 1, 1, 0, 1
 
 function round() {
-    function humanChooses() {
+    function roundEval() {
         switch (humanChoice) {
-            case 'rock' // FINISH
+            case 'rock':
+                if (compChoice === 'paper') {
+                    compCount++;
+                } else if (compChoice === 'scissors') {
+                    humanCount++;
+                }
+                break;
+            case 'paper':
+                if (compChoice === 'rock') {
+                    humanCount++;
+                } else if (compChoice === 'scissors') {
+                    compCount++;
+                }
+                break;
+            case 'scissors':
+                if (compChoice === 'rock') {
+                    compCount++;
+                } else if (compChoice === 'paper') {
+                    humanCount++;
+                }
+                break;
+            default:
+                compCount++;
+                break;
         }
     };
     function newRound() {
@@ -53,26 +78,34 @@ function round() {
     console.log('Round count in round():' + roundCount);
     time = 3;
     interval;
+    humanWL = humanCount / compCount;
     document.getElementById('roundCount').textContent = roundCount;
     document.getElementById('humanCount').textContent = humanCount;
     document.getElementById('compCount').textContent = compCount;
     document.getElementById('humanWL').textContent = humanWL;
+    document.getElementById('humanChoice').textContent = humanChoice;
+    document.getElementById('compChoice').textContent = compChoice;
     randNum();
     randomNumber = randNum();
     compChoice = compList[randomNumber]; // decides what the computer chooses
     console.log('Random number:' + randomNumber)
     console.log('Computer choice:' + compChoice);
     };
-
-    if (humanWL < 1.5 && humanWL > 0.5 || humanWL === NaN) {
+    if (end) {
+        window.location.reload;
+    }
+    roundEval();
+    if (humanWL < 4 && humanWL > 0.25 || humanWL === NaN) {
         newRound();
-    } else if (humanWL >= 1.5) {
-        alert('gud');
-    } else if (humanWL) {
-        alert('bad');
+    } else if (humanWL >= 4) {
+        document.getElementById('win').classList.add('visible');
+        end = true;
+    } else if (humanWL <= 0.25) {
+        document.getElementById('lose').classList.add('visible');
+        end = true;
     } else {
-        alert('Javascript error.')
-    } // starts a new round if no one has won
+        alert('Javascript error.');
+    } // starts a new round if no one has won. Ends game if game is won or lost. A winning status must be maintaine for a round in order to win.
     humanChoice = undefined;
 };
 
